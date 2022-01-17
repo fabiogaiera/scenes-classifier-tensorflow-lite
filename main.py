c408e6d7e2700138f69809ef8f17f02e6d2b60c0
Fabio Gaiera <fabio.gaiera@outlook.com>
2022-01-17 13:00:15 +0100

    Modified main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message":"Hello World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def main():
+    content = """
+    <marquee width="525" behavior="alternate"><h1 style="color:red;font-family:Arial">Please Upload Your Scenes!</h1></marquee>
+    """
+    return content
