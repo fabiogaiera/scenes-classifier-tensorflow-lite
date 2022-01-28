6d14b82576b43a6f533812ec94ad50868e3bd528
Fabio Gaiera <fagaiera@gmail.com>
2022-01-28 20:37:28 +0100

    Restored library versions

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from typing import List
 
 interpreter = tf.lite.Interpreter(model_path='static/model/model.tflite')
 interpreter.allocate_tensors()
-
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
