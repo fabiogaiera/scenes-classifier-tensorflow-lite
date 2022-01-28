db961e421be516ac8639802c015b05f3fd97c3dc
Fabio Gaiera <fagaiera@gmail.com>
2022-01-28 21:39:00 +0100

    Modified app

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi.staticfiles import StaticFiles
 from PIL import Image
 from typing import List
 
-# TensorFlow Lite stuff
+# TensorFlow Lite initialization
 
 interpreter = tf.lite.Interpreter(model_path='static/model/model.tflite')
 interpreter.allocate_tensors()
@@ -16,6 +16,49 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 classes = ['building', 'forest', 'glacier', 'mountain', 'sea', 'street']
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+@app.post("/", response_class=HTMLResponse)
+def main():
+    files = [file + '.jpg' for file in classes]
+    paths = ['static/images/' + x for x in files]
+    empty_column_labels = []
+    content = html_open_tag + head_tag + body_center_open_tag + \
+              marquee_home_tag + heading + table_tag(paths, classes, empty_column_labels) + \
+              form_tag + body_center_close_tag + html_close_tag
+
+    return content
+
+
+@app.post("/predictions/", response_class=HTMLResponse)
+async def render_predictions(files: List[UploadFile] = File(...)):
+    pillow_images = []
+    predictions = []
+    new_size = (150, 150)
+
+    for file in files:
+        f = await file.read()
+        img = Image.open(io.BytesIO(f)).resize(new_size)
+        pillow_images.append(img)
+        predicted_class = predict(img)
+        predictions.append(predicted_class)
+
+    names = [file.filename for file in files]
+
+    for image, name in zip(pillow_images, names):
+        image.save('static/' + name)
+
+    image_paths = ['static/' + name for name in names]
+    column_labels = ["Image", "Prediction"]
+
+    return html_open_tag + head_tag + body_center_open_tag + marquee_prediction_tag + \
+           table_tag(image_paths, predictions,
+                     column_labels) + form_prediction_tag + body_center_close_tag + html_close_tag
+
 
 def predict(image):
     # [1 150 150 3]
@@ -27,67 +70,17 @@ def predict(image):
     return classes[np.argmax(output_data)]
 
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/", response_class=HTMLResponse)
-def main():
-    files = ['Building.jpg', 'Forest.jpg', 'Glacier.jpg', 'Mountain.jpg', 'Sea.jpg', 'Street.jpg']
-
-    paths = ['static/images/' + x for x in files]
-
-    classes_list = ['Building', 'Forest', 'Glacier', 'Mountain', 'Sea', 'Street']
-
-    empty_column_labels = []
-
-    content = html_open_tag + head_tag + body_center_open_tag + marquee_tag + heading + table_tag(paths, classes_list,
-                                                                                                  empty_column_labels) + form_tag + body_center_close_tag + html_close_tag
-
-    return content
-
-
-@app.post("/predictions/", response_class=HTMLResponse)
-async def get_predictions(files: List[UploadFile] = File(...)):
-    
-    pillow_images = []
-    predictions = []
-    new_size = (150, 150)
-    
-    for file in files:
-        f = await file.read()
-        img = Image.open(io.BytesIO(f)).resize(new_size)
-        pillow_images.append(img)
-        predicted_class = predict(img)
-        predictions.append(predicted_class)
-
-
-    names = [file.filename for file in files]
-
-    for image, name in zip(pillow_images, names):
-        image.save('static/' + name)
-
-    image_paths = ['static/' + name for name in names]
-
-    column_labels = ["Image", "Prediction"]
-
-
-    return html_open_tag + table_tag(image_paths, predictions, column_labels) + html_close_tag
-
-
 # Static tags
 
 html_open_tag = """<html>"""
 
 head_tag = """<head><meta name="viewport" content="width=device-width, initial-scale=1"/></head>"""
 
+heading = """<h3 style="font-family:Arial">The application will try to predict which of these categories they are:</h3> """
+
 body_center_open_tag = """<body><center>"""
 
-marquee_tag = """<marquee width="680" behavior="scroll"><h1 style="color:blue;font-family:Arial">Please upload your 
-scenes</h1></marquee> """
-
-heading = """<h3 style="font-family:Arial">The application will try to predict which of these categories they 
-are:</h3> """
+marquee_home_tag = """<marquee width="680" behavior="scroll"><h1 style="color:blue;font-family:Arial">Please upload your scenes</h1></marquee> """
 
 form_tag = """
             <form action="/predictions/" enctype="multipart/form-data" method="post">
@@ -100,19 +93,24 @@ body_center_close_tag = """</center></body>"""
 
 html_close_tag = """</html>"""
 
+marquee_prediction_tag = """<marquee width="680" behavior="scroll"><h1 style="color:blue;font-family:Arial">The predictions are as follows</h1></marquee>"""
+
+form_prediction_tag = """<br><form method="post" action="/"><button type="submit">Home</button></form>"""
+
 
 # Dynamic tag
-def table_tag(image_paths, classes, column_labels):
+
+def table_tag(image_paths, classes_list, column_labels):
     s = '<table align="center">'
 
     if column_labels:
-        s += '<tr><th><h4 style="font-family:Arial">' + column_labels[
-            0] + '</h4></th> <th><h4 style="font-family:Arial">' + column_labels[1] + '</h4></th></tr>'
+        s += '<tr><th><h4 style="font-family:Arial">' + column_labels[0] + '</h4></th> <th><h4 ' \
+                                                                           'style="font-family:Arial">' + \
+             column_labels[1] + '</h4></th></tr> '
 
-    for name, image_path in zip(classes, image_paths):
-        s += '<tr><td><img height="80" src="/' + image_path + '" ></td>'
-        s += '<td style="text-align:center">' + name + '</td></tr>'
+    for name, image_path in zip(classes_list, image_paths):
+        s += '<tr><td><img height="80" src="/' + image_path + '" ></td> <td style="text-align:center">' + name + '</td></tr>'
 
-    s += '</table>'
+    s += '</table><br>'
 
     return s
